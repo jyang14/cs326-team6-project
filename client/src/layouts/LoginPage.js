b8d20a87fcb9b34df2ea3ded7e8750861c2d5959
Jinchao Yang <jinchao2000@gmail.com>
2019-11-12 14:16:29 -0500

    reformat some javascript

--- client/src/layouts/LoginPage.js
+++ client/src/layouts/LoginPage.js
@@ -3,11 +3,7 @@ import React from 'react'
 import './LoginPage.css'
 
 function LoginPage () {
-  return (
-    <div>
-      TODO: Login Page
-    </div>
-  )
+  return <div>TODO: Login Page</div>
 }
 
 export default LoginPage
