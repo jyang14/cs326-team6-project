1f8c4a9b1a4fefc88c5f538dbfcda6deecec8523
Jinchao Yang <jinchao2000@gmail.com>
2019-11-14 14:14:49 -0500

    add storybook for client

---
+++ client/src/components/FoodCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Card from '@material-ui/core/Card'
+
+function FoodCard () {
+  return (
+    <Card>
+      TODO: Fill me
+    </Card>
+  )
+}
+
+export default FoodCard
