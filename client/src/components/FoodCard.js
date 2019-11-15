6bbc10b50dd434286cbbc818b01fd4d8a9c013e8
Jinchao Yang <jinchao2000@gmail.com>
2019-11-15 16:38:56 -0500

    add more ui components

--- client/src/components/FoodCard.js
+++ client/src/components/FoodCard.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
 function FoodCard () {
   return (
     <Card>
-      TODO: Fill me
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='h2'>
+          FOOD_NAME
+        </Typography>
+        <Typography variant='body1' color='textSecondary' component='p'>
+          Expires: EXPIRATION DATE
+        </Typography>
+        <Typography variant='body2' color='textSecondary' component='p'>
+          LOCATION
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small' color='primary'>
+          Edit
+        </Button>
+        <Button size='small' color='primary'>
+          Remove
+        </Button>
+      </CardActions>
     </Card>
   )
 }
--- client/src/components/SearchBar.stories.js
+++ client/src/components/SearchBar.stories.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import SearchBar from './SearchBar'
 
-export default { title: 'SearchBar'}
+export default { title: 'SearchBar' }
 
 export const defaultView = () => <SearchBar />
